29343b1c598a5603b33293a935d452814c393582
WHJR-Grade12 <91465322+WHJR-G12-Github@users.noreply.github.com>
2021-10-08 15:44:39 +0530

    Create template_c3_saa2.py

---
+++ template_c3_saa2.py
@@ -0,0 +1,12 @@
+# Create a variable 'x' 
+x = 
+# Create a variable 'y' 
+y = 
+# Printing the values of 'x' and 'y' before swapping
+print("Before Swapping:",x,y)
+
+# Swapping - Interchanging the values of x and y
+x,y = 
+
+# Printing the values of 'x' and 'y' after swapping
+print("After Swapping:",x,y)
